402077261b630fdf6b835426102ee9a93c874425
kojoankra94 <120052998+kojoankra94@users.noreply.github.com>
2025-03-09 21:34:53 -0500

    first commit

--- cypress/e2e/learning/tutorials/First.cy.js
+++ cypress/e2e/learning/tutorials/First.cy.js
@@ -5,6 +5,10 @@ describe('Validate Login Functionality', () =>
     it('Login with valid credentials', () => 
     {
         cy.visit('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login')
+        cy.get('#txtUsername').type('Admin')
+        cy.get('#txtPassword').type('admin123')
+        cy.get('#btnLogin').click()
+        cy.get('#welcome').should('be.visible')
        
     });
 
